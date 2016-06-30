5ab52aee9c5c96b4f40a0d13d0706e85b35b6c44
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2016-06-30 18:19:27 +0300

    Fix tabulation in some of the files

--- src/dependencies.php
+++ src/dependencies.php
@@ -5,17 +5,17 @@ $container = $app->getContainer();
 
 // view renderer
 $container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+  $settings = $c->get('settings')['renderer'];
+  return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
 // monolog
 $container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
-    return $logger;
+  $settings = $c->get('settings')['logger'];
+  $logger = new Monolog\Logger($settings['name']);
+  $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+  $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+  return $logger;
 };
 
 // pdo
