8a00f47151b747fd37aa91d429c76a6ec0e27bc4
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-12-05 15:22:13 +0100

    Display 40 items per page instead of 20
    This makes the asset library easier to browse by requiring less clicks
    to browse between pages (since there will be fewer pages overall).
    
    The Godot editor is able to handle this change seamlessly.

--- src/routes/asset_edit.php
+++ src/routes/asset_edit.php
@@ -322,7 +322,7 @@ $app->get('/asset/edit', function ($request, $response, $args) {
     $filter = '%';
     $username = '%';
     $statuses = [];
-    $page_size = 20;
+    $page_size = 40;
     $max_page_size = 500;
     $page_offset = 0;
     if (isset($params['asset'])) {
